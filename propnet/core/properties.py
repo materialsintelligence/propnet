9746a028180042cc802144575d430eeb7ff3fb98
dmrdjenovich <dmrdjenovich@berkeley.edu>
2017-10-13 14:51:46 -0700

    Changed NamedTuples to classes with constructors for automatic error checking and simplified extensibility.
    The idea is to allow for the inclusion of convenience constructors later on that won't have to be modified at all even if the definitions of the classes are later modified. Having the class's main constructor perform error checking allows for many methods to use those capabilities without having to reinvent the wheel. This is useful if we want to later construct objects from non-dictionaries.

--- propnet/core/properties.py
+++ propnet/core/properties.py
@@ -4,60 +4,70 @@ from typing import *
 from propnet import logger, ureg
 from pybtex.database.input.bibtex import Parser
 
-# metadata associated with each PropertyType
-PropertyMetadata = NamedTuple('PropertyMetadata', [('units', ureg.Quantity),
-                                                   ('display_names', List[str]),
-                                                   ('display_symbols', List[str]),
-                                                   ('dimension', List),
-                                                   ('test_value', np.ndarray),
-                                                   ('comment', str)])
+class PropertyMetadata :
+#Class storing the complete description of a property.
+
+    expected_props = ['id', 'units', 'display_names', 'display_symbols', 'dimension', 'test_value', 'comment']
+
+    def __init__ (self, id : str, units : ureg.Quantity, display_names : List[str], display_symbols : List[str],
+                  dimension : List[int], test_value : np.ndarray, comment : str) :
+        """
+            Parses and validates a series of inputs into a PropertyMetadata tuple, a format that PropNet expects.
+            Parameters correspond exactly with those of a PropertyMetadata tuple.
+            :param id: string ASCII identifying the property uniquely as an internal identifier.
+            :param units: units of the property, supported by the Pint package.
+            :param display_names: list of strings giving possible human-readable names for the property.
+            :param display_symbols: list of strings giving possible human-readable symbols for the property.
+            :param dimension: list giving the order of the tensor as the length, and number of dimensions as individual
+                              integers in the list.
+            :param test_value: a sample value of the property, reasonable over a wide variety of contexts.
+            :param comment: any useful information on the property including its definitions and possible citations.
+            :return: Properly-initialized PropertyMetadata instance.
+        """
+        if not id.isidentifier() or not id.islower() :
+            raise ValueError("The canonical name ({}) is not valid.".format(id))
+        self.id = id
+        self.units = ureg.Quantity.from_tuple(units)
+        if display_names == None or len(display_names) == 0:
+            raise ValueError("Insufficient display names for ({}).".format(id))
+        self.display_names = display_names
+        if display_symbols == None or len(display_symbols) == 0:
+            raise ValueError("Insufficient display symbols for ({}).".format(id))
+        self.display_symbols = display_symbols
+        try :
+            np.zeros(dimension)
+        except :
+            raise ValueError("Dimensions provided for ({}) are invalid.".format(id))
+        self.dimension = dimension
+        if not np.any(test_value) :
+            logger.warn("Test value for {} is zero. Please change to a more appropriate test value.")
+        self.test_value = test_value
+        self.comment = comment
+
+    @staticmethod
+    def from_dict (d: dict) -> PropertyMetadata :
+        """
+        Convenience constructor. Creates a PropertyMetadata instance from a dictionary with appropriate
+        key-value pairs.
+        :param d: dict containing the information necessary to construct a PropertyMetadata object.
+        :return: Properly-initialized PropertyMetadata instance.
+        """
+        for key in PropertyMetadata.expected_props :
+            if key not in d.keys() :
+                raise ValueError("Provided Dictionary is missing input argument: " + key)
+        return PropertyMetadata(**d)
 
 
-def parse_property(property: dict) -> (str, PropertyMetadata):
-    """
-    Parse and validate a dict (e.g. from a .yaml file) into
-    a named tuple in the format that Propnet expects.
-    :param property: dict containing property information
-    :return:
-    """
+class Property :
+#Class storing the value of a property in a given context.
 
-    name = property['name']
-    # using the name as a key, no need to keep it in metadata too
-    del property['name']
-
-    # check canonical name
-    if not name.isidentifier() or \
-            not name.islower():
-        raise ValueError("The canonical name ({}) is not valid."
-                         .format(name))
-
-    # check units supported by pint
-    units = ureg.Quantity.from_tuple(property['units'])
-    property['units'] = units
-
-    # check required fields
-    if len(property['display_names']) == 0:
-        raise ValueError("Please provide at least one display name for {}."
-                         .format(name))
-    if len(property['display_symbols']) == 0:
-        raise ValueError("Please provide at least one display symbol for {}."
-                         .format(name))
-
-    # check test value is valid
-    test_value = np.array(property['test_value'])
-    if not np.any(test_value):
-        logger.warn("Test value for {} is zero, please change to a more appropriate test value."
-                    .format(name))
-
-    # check dimensions are valid
-    try:
-        empty_array = np.zeros(property['dimension'])
-    except:
-        raise ValueError("Invalid dimensions for {}.".format(name))
-
-    return (name, PropertyMetadata(**property))
-
-
-# TODO: proper Property class, this is a placeholder
-Property = NamedTuple('Property', [('property_type', str),
-                                   ('value', Any)])
+    def __init__ (self, type : PropertyMetadata, value : Any, comment : str) :
+        """
+        Parses inputs for constructing a Property object.
+        :param type: pointer to an existing PropertyMetadata object, identifies the type of data stored in the property.
+        :param value: value of the property
+        :param comment: important information relative to the property, including sourcing.
+        """
+        self.type = type
+        self.value = value
+        self.comment = comment
