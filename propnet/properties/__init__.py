3539b6f4de158d72fa08be4deea887ed9d700d58
dmrdjenovich <dmrdjenovich@berkeley.edu>
2017-10-13 15:19:48 -0700

    Changed the reading of properties .yaml files to accommodate the new class structure of PropertyMetadata.
    Made property_metadata a dictionary in the global scope to accommodate constant-time lookup of PropertyMetadata by name.

--- propnet/properties/__init__.py
+++ propnet/properties/__init__.py
@@ -5,22 +5,26 @@ from os import path
 from monty.serialization import loadfn
 from propnet import logger
 
-from propnet.core.properties import PropertyMetadata, parse_property
+from propnet.core.properties import PropertyMetadata
 
-# auto loading of allowed properties
+# Auto loading of all allowed properties
 
-files: List[str] = glob(path.join(path.dirname(__file__), '../properties/*.yaml'))
+# Stores all loaded properties as PropertyMetadata instances in a dictionary in the global scope, mapped from
+# their names.
+property_metadata: dict[str : PropertyMetadata] = {}
 
-property_metadata: List[Tuple[str, PropertyMetadata]] = []
-for f in files:
+property_metadata_files: List[str] = glob(path.join(path.dirname(__file__), '../properties/*.yaml'))
+
+for f in property_metadata_files:
     try:
-        property_metadata.append(parse_property(loadfn(f)))
+        storing = PropertyMetadata.from_dict(loadfn(f))
+        property_metadata[storing.name] = storing
     except Exception as e:
         logger.error('Failed to parse {}, {}.'.format(path.basename(f), e))
 
 # using property names for Enum, conventional to have all upper case
 # but using all lower case here
-PropertyType: Enum = Enum('PropertyType', property_metadata)
+PropertyType: Enum = Enum('PropertyType', property_metadata.keys())
 
-# for convenience
-all_property_names: Tuple[str] = tuple(p.name for p in PropertyType)
+# Stores all loaded properties' names in a tuple in the global scope.
+all_property_names: Tuple[str] = tuple(p for p in property_metadata.keys())
