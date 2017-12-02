71f09474331b660143ecac0d67835f78bfcb6439
JPalakapilly <jjp126@gmail.com>
2017-12-02 15:44:28 -0800

    working on mpinterface

--- propnet/core/mpInterface.py
+++ propnet/core/mpInterface.py
@@ -1,13 +1,36 @@
 from pymatgen import MPRester
+#from propnet.core.properties import Property
 
-def importProps(mpid = "mp-1234", propList = ["final_energy","density"]):
+
+
+def importProps(mpid = ["mp-1234"], propList = ["final_energy","density"]):
     m = MPRester("sNxknEySUTz2owRL")
     data = m.query(criteria={"task_id": mpid}, properties=propList)
     return(data)
 
-a = importProps(mpid = "mp-1243" )[0]
+#maps propnet property names to mp (mapidoc) property 'location'
+mppropname = {
+    'poisson_ratio': 'elasticity.poisson_ratio',
+    'bulk_modulus': 'elasticity.K_Voigt_Reuss_Hill',
+    'elastic_tensor_voigt': 'elasticity.elastic_tensor',
+    'relative_permittivity': 'diel.poly_total',
+    'density': 'density',
+    'refractive_index': 'diel.n',
+    'energy_above_hull': 'e_above_hull'
+
+
+
+}
+
+
+
+mp_ids = ["mp-1243","mp-1234"]
+proplist = list(mppropname.keys());
+
+a = importProps(mpid = {'$in':mp_ids} )
 props = []
 #list of properties
+print(a)
 for key in a:
     p = Property(key, a[key], )
     #make property
