0a557a345887a9ddfda2581efc99c67a3736f564
dmrdjenovich <dmrdjenovich@berkeley.edu>
2017-12-05 14:27:59 -0800

    Minor PEP8 cleanups

--- propnet/core/mpInterface.py
+++ propnet/core/mpInterface.py
@@ -1,9 +1,8 @@
 from pymatgen import MPRester
-from propnet.core.properties import Property,
+from propnet.core.properties import Property
 from propnet.properties import PropertyType, property_metadata
 
-
-#maps propnet property names to mp (mapidoc) property 'location'
+# maps propnet property names to mp (mapidoc) property 'location'
 mp_from_propnet_name_mapping = {
     'poisson_ratio': 'elasticity.poisson_ratio',
     'bulk_modulus': 'elasticity.K_Voigt_Reuss_Hill',
@@ -23,6 +22,7 @@ propnet_from_mp_name_mapping = {v:k for k,v in mp_from_propnet_name_mapping.item
 mp_prop_list = list(propnet_from_mp_name_mapping.keys()) + ['task_id']
 propnet_prop_list = list(mp_from_propnet_name_mapping.keys())
 
+
 def import_all_props(mp_ids):
     """
     Given a list of materials ids, returns a list of all available properties
@@ -31,6 +31,7 @@ def import_all_props(mp_ids):
     :return: list of lists containing all possible materials properties by mp_id
     """
     return import_props(mp_ids, mp_prop_list)
+
 
 def import_props(mp_ids, prop_list):
     """
@@ -44,15 +45,15 @@ def import_props(mp_ids, prop_list):
     m = MPRester()
     query = m.query(criteria={"task_id": {'$in':mp_ids}}, properties=prop_list)
     print(query)
-    #properties of all mp-ids inputted; list of lists
+    # properties of all mp-ids inputted; list of lists
     properties = []
     for data in query:
-        #properties of one mp-id
+        # properties of one mp-id
         mat_properties = []
         for key in data:
             if not data[key] is None:
                 prop_type = property_metadata[propnet_from_mp_name_mapping[key]]
-                p = Property(prop_type, data[key], None, [(data['task_id'], '')], None)
+                p = Property(prop_type, data[key], 'Imported from Materials Project', [(data['task_id'], '')], {})
                 mat_properties.append(p)
         properties.append(mat_properties)
     return properties
