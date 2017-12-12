a6ee85ad1c795b64bc0d541a52ca3dca8769e584
Matthew Horton <mkhorton@users.noreply.github.com>
2017-12-12 13:53:54 -0800

    Update models layout for when equations missing

--- propnet/web/layouts_models.py
+++ propnet/web/layouts_models.py
@@ -6,6 +6,7 @@ from propnet.web.utils import references_to_markdown
 
 import propnet.models as models
 
+
 # layouts for model detail pages
 
 def model_image_component(model):
@@ -19,7 +20,8 @@ def model_image_component(model):
     """
 
     url = "https://robohash.org/{}".format(model.__hash__())
-    return html.Img(src=url, style={'width': 150, 'border-radius':'50%'})
+    return html.Img(src=url, style={'width': 150, 'border-radius': '50%'})
+
 
 def model_layout(model_name):
     """Create a Dash layout for a provided model.
@@ -38,7 +40,7 @@ def model_layout(model_name):
 
     if model.tags:
         tags = html.Ul(className="tags", children=[
-                html.Li(tag, className="tag") for tag in model.tags
+            html.Li(tag, className="tag") for tag in model.tags
         ])
     else:
         tags = None
@@ -47,7 +49,6 @@ def model_layout(model_name):
         html.Span('model-id', className='model'),
         html.Span('{}'.format(model.model_id))
     ])
-
 
     title = html.Div(className='row', children=[
         html.Div(className='three columns', style={'text-align': 'right'}, children=[
@@ -69,21 +70,26 @@ def model_layout(model_name):
         html.Div(className='row', children=[
             html.Div(className='two columns', children=[str(symbol)]),
             html.Div(className='ten columns', children=[dcc.Link(get_display_name(property_name),
-                                                                 href='/property/{}'.format(property_name))])
+                                                                 href='/property/{}'.format(
+                                                                     property_name))])
         ])
         for symbol, property_name in model.symbol_mapping.items()
     ])
 
-    # TODO: change equations to property
-    equations_layout = html.Div(children=[
-        html.Div(className='row', children=[equation])
-        for equation in model.equations
-    ])
-
-    #method = model.method TODO: add below description
+    # method = model.method TODO: add below description
 
     # TODO
     breadcrumb = html.Div()
+
+    if hasattr(model, 'equations'):
+        # TODO: change equations to property
+        equations_layout = html.Div(children=[html.H6('Equations'),
+                                              html.Div(children=[
+                                                  html.Div(className='row', children=[equation])
+                                                  for equation in model.equations
+                                              ])])
+    else:
+        equations_layout = html.Div()
 
     return html.Div([
         breadcrumb,
@@ -93,7 +99,6 @@ def model_layout(model_name):
         references,
         html.H6('Symbols'),
         symbols_layout,
-        html.H6('Equations'),
         equations_layout,
         html.H6('What this model does'),
         dcc.Markdown(model.description),
@@ -106,7 +111,6 @@ def model_layout(model_name):
 
 model_links = []
 for model_name in models.all_model_names:
-
     # instantiate model from name
     model = getattr(models, model_name)()
 
