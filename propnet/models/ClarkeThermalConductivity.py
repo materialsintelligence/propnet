f3f3094365c190f3201a68dd49d3b0475a3f723b
Matthew Horton <mkhorton@users.noreply.github.com>
2018-01-24 11:54:23 -0800

    Add .yaml files for Models

--- propnet/models/ClarkeThermalConductivity.py
+++ propnet/models/ClarkeThermalConductivity.py
@@ -1,57 +1,12 @@
 from propnet.core.models import AbstractModel
 
-from pymatgen.analysis.elasticity import ElasticTensor
+from pymatgen.analysis.elasticity.elastic import ElasticTensor
 
 class ClarkeThermalConductivity(AbstractModel):
-    """ """
 
-    @property
-    def title(self):
-        """ """
-        return "Calculate Clarke thermal conductivity"
+   def evaluate(self, symbols):
 
-    @property
-    def tags(self):
-        """ """
-        return ["thermal"]
+       tensor = ElasticTensor.from_voigt(symbols["C_ij"])
+       structure = symbols["_structure"]
 
-    @property
-    def description(self):
-        return """This model currently uses the implementation in pymatgen."""
-
-    @property
-    def references(self):
-        """ """
-        return ""
-
-    @property
-    def symbol_mapping(self):
-        """ """
-        return {
-            'C_ij': 'elastic_tensor_voigt',
-            'structure': 'structure',
-            't': 'thermal_conductivity'
-        }
-
-
-    @property
-    def connections(self):
-        """ """
-        return {
-            't': {'C_ij', 'structure'}
-        }
-
-    def evaluate(self,
-                 symbols_and_values_in,
-                 symbol_out):
-        """
-
-        Args:
-          symbols_and_values_in:
-          symbol_out:
-
-        Returns:
-
-        """
-
-        return
+       return {'t': tensor.clarke_thermalcond(structure)}
