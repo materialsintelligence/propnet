88c5599c6ea8476dc2847e2c13339a40d8602f12
dmrdjenovich <dmrdjenovich@berkeley.edu>
2018-02-16 15:56:57 -0800

    First commit to Testing_and_iPython

--- propnet/core/tests/test_models.py
+++ propnet/core/tests/test_models.py
@@ -13,9 +13,7 @@ from propnet.core.models import *
 class ModelTest(unittest.TestCase):
 
     def testInstantiateAllModels(self):
-
         models_to_test = []
-
         for model_name in all_model_names:
             try:
                 model = getattr(models, model_name)()
@@ -24,24 +22,15 @@ class ModelTest(unittest.TestCase):
                 raise Exception('Failed to load model {}: {}'.format(model_name, e))
 
     def testEvaluate(self):
-
         test_data = glob(os.path.join(os.path.dirname(__file__), '../../models/test_data/*.json'))
-
         for f in test_data:
-
             model_name = os.path.splitext(os.path.basename(f))[0]
-
             if model_name in all_model_names:
-
                 model = getattr(models, model_name)()
                 model_test_data = loadfn(f)
-
                 for d in model_test_data:
-
                     model_outputs = model.evaluate(d['inputs'])
-
                     for k, v in d['outputs'].items():
                         self.assertAlmostEqual(model_outputs[k], v)
-
             else:
-                raise ValueError("Test file has been misnamed: {}".format(model_name))
+                raise ValueError("Model not found: {}".format(model_name))
