9377cb1c42231820518088ce352310e1ef725631
Matthew Horton <mkhorton@users.noreply.github.com>
2018-04-01 14:51:24 +0100

    Clean up default models, symbols __init__

--- propnet/symbols/__init__.py
+++ propnet/symbols/__init__.py
@@ -1,49 +1,29 @@
 import os
 
-from typing import *
 from glob import glob
-from enum import Enum
 from monty.serialization import loadfn
 from propnet import logger
 
-from propnet.core.symbols import SymbolMetadata
-
-# TODO: clean up this file, move as much to propnet.core.properties as possible
+from propnet.core.symbols import SymbolType
 
 # Auto loading of all allowed properties
 
-# Stores all loaded properties as PropertyMetadata instances in a dictionary, mapped to
-# their names
-_symbol_metadata: Dict[str, SymbolMetadata] = {}
+# stores all loaded properties as PropertyMetadata instances in a dictionary,
+# mapped to their names
+DEFAULT_SYMBOL_TYPES = {}
 
-_symbol_metadata_files: List[str] = glob(os.path.join(os.path.dirname(__file__),
-                                                    '../symbols/**/*.yaml'),
-                                         recursive=True)
+_DEFAULT_SYMBOL_TYPE_FILES = glob(os.path.join(os.path.dirname(__file__),
+                                           '../symbols/**/*.yaml'),
+                                  recursive=True)
 
-for f in _symbol_metadata_files:
+for f in _DEFAULT_SYMBOL_TYPE_FILES:
     try:
-        metadata = SymbolMetadata.from_dict(loadfn(f))
-        _symbol_metadata[metadata.name] = metadata
-        if "{}.yaml".format(metadata.name) not in f:
+        symbol_type = SymbolType.from_dict(loadfn(f))
+        DEFAULT_SYMBOL_TYPES[symbol_type.name] = symbol_type
+        if "{}.yaml".format(symbol_type.name) not in f:
             raise ValueError('Name/filename mismatch in {}'.format(f))
     except Exception as e:
         logger.error('Failed to parse {}, {}.'.format(os.path.basename(f), e))
 
-# using property names for Enum, conventional to have all upper case
-# but using all lower case here
-SymbolType: Enum = Enum('SymbolType', [(k, v) for k, v in _symbol_metadata.items()])
-
 # Stores all loaded properties' names in a tuple in the global scope.
-all_symbol_names: Tuple[str] = tuple(p for p in _symbol_metadata.keys())
-
-
-def get_display_name(symbol_name):
-    """Convenience function
-
-    Args:
-      symbol_name: 
-
-    Returns:
-
-    """
-    return SymbolType[symbol_name].value.display_names[0]
+DEFAULT_SYMBOL_TYPE_NAMES = tuple(DEFAULT_SYMBOL_TYPES.keys())
