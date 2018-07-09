9f7a8fd801e8d5fefb9d8b42630edcf2cc283d85
montoyjh <montoyjh@gmail.com>
2018-07-09 09:37:15 -0700

    add placeholders for mp rester tests

--- propnet/ext/tests/test_matproj.py
+++ propnet/ext/tests/test_matproj.py
@@ -1,14 +1,27 @@
 import unittest
 
-from propnet.ext.matproj import *
+from propnet.ext.matproj import MPRester
 
 
-class MatProjTest(unittest.TestCase):
+class MPResterTest(unittest.TestCase):
 
     def test_load_properties(self):
-
         mpid = 'mp-1153'
         mpr = MPRester()
         mat = mpr.get_material_for_mpid(mpid)
-
         self.assertIn('structure', mat.get_symbols())
+
+    def test_get_mpid_from_formula(self):
+        pass
+
+    def test_get_properties_for_mpids(self):
+        pass
+
+    def test_get_properties_for_mpid(self):
+        pass
+
+    def test_get_materials_for_mpids(self):
+        pass
+
+    def test_get_materials_for_mpid(self):
+        pass
