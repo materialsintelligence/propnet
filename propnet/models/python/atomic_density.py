41860a87b93714864dc9bf90f98f431f463b777b
montoyjh <montoyjh@gmail.com>
2018-07-08 18:06:50 -0700

    add python model

---
+++ propnet/models/python/atomic_density.py
@@ -0,0 +1,18 @@
+from propnet import ureg
+
+def evaluate_atomic_density(s):
+    p = ureg.Quantity.from_tuple(s.num_sites / s.volume,
+                                 [['angstroms', -3]])
+    return {'p': p}
+
+
+config = {
+    "name": "Atomic density model",
+    "function": evaluate_atomic_density,
+    "tags": ["mechanical"],
+    "references": [],
+    "symbol_mapping": {"s": "structure",
+                       "p": "atomic_density"},
+    "connections": [{"inputs": ["s"],
+                     "outputs": ["p"]}]
+}
