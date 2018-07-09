5f7962efc0ac1a0500f2f0aa981382548083547e
montoyjh <montoyjh@gmail.com>
2018-07-09 09:37:42 -0700

    add load_material test in web app

--- propnet/web/tests/test_web.py
+++ propnet/web/tests/test_web.py
@@ -1,5 +1,6 @@
 import unittest
 from propnet.web.app import app
+from propnet.web.app import retrieve_material
 
 routes = [
     '/',
@@ -35,6 +36,22 @@ class WebTest(unittest.TestCase):
     def test_utilities(self):
         utilities = self.client.get('/utilities')
         self.assertEqual(utilities.status_code, 200)
+
+    def test_property(self):
+        properties = self.client.get('/property')
+        self.assertEqual(properties.status_code, 200)
+
+        app_stress = self.client.get('/property/applied_stress')
+        self.assertEqual(properties.status_code, 200)
+
+    def test_load_material(self):
+        # Test retrieve material method
+        material_no_derive = retrieve_material(1, None, "Ag", aggregate=False)
+        self.assertEqual(material_no_derive.status_code, 200)
+        material_derive = retrieve_material(1, 1, "Ag", False)
+        self.assertEqual(material_derive.status_code, 200)
+        material_aggregate = retrieve_material(1, 1, "Ag", aggregate=True)
+        self.assertEqual(material_aggregate.status_code, 200)
 
     def tearDown(self):
         pass
