69ca4ad31660fb01ceb753e0e7a62e7b31234a0e
montoyjh <montoyjh@gmail.com>
2018-09-04 17:22:36 -0700

    add cli and setup

---
+++ propnet/cli.py
@@ -0,0 +1,70 @@
+import argparse
+from propnet.core.models import EquationModel, PyModel
+from propnet.models import DEFAULT_MODEL_DICT
+from monty.serialization import loadfn
+
+__author__ = "Joseph Montoya <montoyjh@lbl.gov>"
+__version__ = 0.1
+__date__ = "Sep 4 2018"
+
+
+def run_web_app(debug=False):
+    pass
+
+def validate(args):
+    if args.name is not None:
+        import nose; nose.tools.set_trace()
+        model = DEFAULT_MODEL_DICT[args.name]
+        if not args.test_data:
+            model.validate_from_preset_test()
+            print("Model validated with test data")
+            return True
+    elif args.file is not None:
+        if args.file.endswith(".yaml"):
+            EquationModel.from_file(args.file)
+        elif args.file.endswith(".py"):
+            # This should define config
+            l, g = locals().copy(), globals().copy()
+            with open(args.file) as f:
+                code = compile(f.read(), args.file, 'exec')
+                exec(code, globals())
+            config = globals().get('config')
+            model = PyModel(**config)
+
+    if args.test_data is not None:
+        td_data = loadfn(args.test_data)
+        for td_datum in td_data:
+            model.test(**td_datum)
+        print("{} validated with test data".format(model.name))
+    return True
+
+
+def main():
+    parser = argparse.ArgumentParser(description="""
+    propnet is a command-line interface to the propnet python package""",
+                                     epilog="""
+    Author: Joseph Montoya
+    Version: {}
+    Last updated: {}""".format(__version__, __date__))
+    subparsers = parser.add_subparsers()
+
+    # Web runner
+    parser_web = subparsers.add_parser("run_web", help="Run web server")
+    parser_web.add_argument("-d", "--debug", help="Run in debug mode",
+                            action="store_true")
+    parser_web.set_defaults(func=run_web_app)
+
+    # Validate models
+    parser_validate = subparsers.add_parser(
+        "validate", help="validate models")
+    parser_validate.add_argument("-n", "--name", help="Default model name")
+    parser_validate.add_argument("-f", "--file", help="Non-default model filename")
+    parser_validate.add_argument("-t", "--test_data", help="Test data file")
+    parser_validate.set_defaults(func=validate)
+
+    args = parser.parse_args()
+    args.func(args)
+
+
+if __name__ == "__main__":
+    main()
