05982e17c4c65c1986128966b218cab0e251bc8b
montoyjh <montoyjh@gmail.com>
2018-10-07 10:57:04 -0700

    clean up some stuff

--- propnet/core/tests/test_builder.py
+++ propnet/core/tests/test_builder.py
@@ -10,21 +10,11 @@ from propnet.core.builder import PropnetBuilder
 
 class BuilderTest(unittest.TestCase):
     def setUp(self):
-        self.materials = MemoryStore()# MongoStore("test_propnet", "materials")
+        self.materials = MemoryStore()
         self.materials.connect()
         materials = loadfn("test_materials.json")
         materials = jsanitize(materials, strict=True, allow_bson=True)
         self.materials.update(materials)
-        # formulas = ["Si", "BaNiO3", "Sn"]
-        # docs = []
-        # for n, formula in enumerate(formulas):
-        #     structure = PymatgenTest.get_structure(formula)
-        #     docs.append({"structure": structure.as_dict(),
-        #                  "task_id": "mp-{}".format(n),
-        #                  "run_type": "GGA+U",
-        #                  "pretty_formula": formula,
-        #                  "band_gap": {"search_gap": {"band_gap": 2.0}}})
-        # self.materials.update(docs)
         self.propstore = MemoryStore()
         self.propstore.connect()
 
@@ -42,18 +32,20 @@ class BuilderTest(unittest.TestCase):
         item = self.materials.query_one(criteria={"pretty_formula": "Cs"})
         builder = PropnetBuilder(self.materials, self.propstore)
         processed = builder.process_item(item)
-        import nose; nose.tools.set_trace()
         self.assertIsNotNone(processed)
 
-    # def test_runner_pipeline(self):
-    #     from monty.serialization import loadfn
-    #     runner = loadfn("../../../runner.json")
-    #     runner.builders[0].connect()
-    #     items = list(runner.builders[0].get_items())
-    #     processed = runner.builders[0].process_item(items[0])
-    #     runner.run()
+    @unittest.skipIf(not os.path.isfile("runner.json"), "No runner file")
+    def test_runner_pipeline(self):
+        from monty.serialization import loadfn
+        runner = loadfn("runner.json")
+        runner.builders[0].connect()
+        items = list(runner.builders[0].get_items())
+        processed = runner.builders[0].process_item(items[0])
+        runner.run()
 
-    # @unittest.skipIf(True, "skip temp")
+    # Just here for reference, in case anyone wants to create a new set
+    # of test materials -jhm
+    @unittest.skipIf(True, "Skipping test materials creation")
     def create_test_docs(self):
         formulas = ["BaNiO3", "Si", "Fe2O3", "Cs"]
         from maggma.advanced_stores import MongograntStore
