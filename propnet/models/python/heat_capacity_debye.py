70ea515aa8766deba5a8792fcbb2a2bac65f7667
montoyjh <montoyjh@gmail.com>
2018-10-09 15:32:50 -0700

    added heat capacity

---
+++ propnet/models/python/heat_capacity_debye.py
@@ -0,0 +1,37 @@
+from scipy.integrate import quad
+from math import exp
+
+def plug_in(symbol_values):
+    t_d = symbol_values['debye_temperature']
+    temp = symbol_values['temperature']
+    t_ratio = temp / t_d
+    integrand = lambda x: (x**4 * exp(x) / (exp(x) - 1)**2)
+    c_v = 9 * 8.314 * t_ratio**3 * quad(integrand, 0, t_ratio**-1)[0]
+    return {"specific_heat_capacity_constant_volume_molar": c_v}
+
+
+DESCRIPTION = """
+Constant volume heat-capacity derived from the debye model
+of solids
+"""
+
+config = {
+    "name": "heat_capacity_debye",
+    "connections": [
+        {
+            "inputs": [
+                "debye_temperature"
+                "temperature"
+            ],
+            "outputs": [
+                "specific_heat_capacity_constant_volume_molar",
+            ]
+        }
+    ],
+    "categories": [
+        "thermal"
+    ],
+    "description": DESCRIPTION,
+    "references": ["doi:10.1016/j.commatsci.2012.10.028"],
+    "plug_in": plug_in
+}
