11c65f508bd10996dc12ef891965f1ed0885221f
montoyjh <montoyjh@gmail.com>
2018-10-10 08:48:35 -0700

    add explicit quantity test

---
+++ propnet/core/tests/test_quantity.py
@@ -0,0 +1,23 @@
+import unittest
+
+from propnet.core.quantity import Quantity
+
+from propnet.symbols import DEFAULT_SYMBOLS
+
+
+class QuantityTest(unittest.TestCase):
+
+    def test_quantity_construction(self):
+        # From custom symbol
+        # From canonical symbol
+        # From custom symbol with constraint
+        # From canonical symbol with constraint
+        pass
+
+    def test_from_weighted_mean(self):
+        # Simple test
+        pass
+
+    def test_is_cyclic(self):
+        # Simple test
+        pass
