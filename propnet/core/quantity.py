01389e08a4921e3554ce7fd5689aa4e6e9241bdd
montoyjh <montoyjh@gmail.com>
2018-11-06 08:47:38 -0800

    move fitting

--- propnet/core/quantity.py
+++ propnet/core/quantity.py
@@ -8,6 +8,7 @@ from propnet import ureg
 from propnet.core.symbols import Symbol
 from propnet.core.provenance import ProvenanceElement
 from propnet.symbols import DEFAULT_SYMBOLS, DEFAULT_SYMBOL_VALUES
+from propnet.models import DEFAULT_MODEL_NAMES
 
 from propnet.core.exceptions import SymbolConstraintError
 
@@ -314,104 +315,3 @@ class Quantity(MSONable):
                    uncertainty=std_dev)
 
 
-# TODO: These fitting utils should likely go in a separate package
-#       that's devoted to the data-science oriented side of propnet
-# TODO: this is very preliminary, could be improved substantially
-def aggregate_quantities(quantities, model_score_dict=None):
-    """
-    Simple method for aggregating a set of quantities
-
-    Args:
-        quantities:
-        model_score_dict:
-
-    Returns:
-
-    """
-    if not all([q.symbol == quantities[0].symbol for q in quantities]):
-        raise ValueError("Quantities passed to aggregate must be same symbol")
-    weights = [get_weight(q, model_score_dict) for q in quantities]
-    result_value = sum(
-        [w * q.value for w, q in zip(weights, quantities)]) / sum(weights)
-    return Quantity(quantities[0].symbol, result_value)
-
-
-def get_weight(quantity, model_score_dict=None):
-    """
-    Gets weight based on scoring scheme
-
-    Args:
-        quantity (Quantity): quantity for which to get weight
-        model_score_dict ({str: float}): dictionary of model names to scores
-
-    Returns:
-        calculated weight for input quantity
-    """
-    if quantity.provenance is None:
-        return 1
-    if model_score_dict is None:
-        return 1
-    weight = model_score_dict.get(quantity.provenance.model)
-    weight *= np.prod(
-        [get_weight(q, model_score_dict) for q in quantity.provenance.inputs])
-    return weight
-
-
-# TODO: Add default graph when this is moved
-def fit_model_scores(materials, benchmarks, models=None,
-                     min_score=0.05, init_scores=None):
-    """
-    Fits a set of model scores to a set of benchmark data
-
-    Args:
-        materials ([Material]): list of materials
-        benchmarks ([{Symbol or str: float}]): list of benchmarks,
-            containing
-        models ([Model or str]): list of models which should have their
-            scores adjusted in the aggregation weighting scheme
-        min_score (float): minimum score to use in weighting
-            scheme, defaults to 0.05
-        init_scores ({str: float}): scores to initialize minimize
-            procedure with
-
-    Returns:
-        {str: float} scores corresponding to those which minimize
-            SSE for the benchmarked dataset
-
-    """
-    # graph = graph #or Graph() Add default graph when this is moved
-    # model_list = graph._models.keys()
-    models = DEFAULT_MODEL_NAMES
-    def f(scores):
-        model_score_dict = {m: max(s, min_score)
-                            for m, s in zip(model_list, scores)}
-        return get_sse(materials, benchmarks, model_score_dict)
-    scores = OrderedDict((m, 1) for m in model_list)
-    scores.update(init_scores or {})
-    result = minimize(f, x0=np.array(scores.values()))
-    vec = [max(s, min_score) for s in result.x]
-    return OrderedDict(zip(model_list, vec))
-
-
-def get_sse(materials, benchmarks, model_score_dict=None):
-    """
-    Function to get the sum squared error of a set of benchmarks
-    with aggregated data from the model scoring scheme above
-
-    Args:
-        materials ([Material]): list of materials to evaluate
-        benchmarks ([{Symbol or str: float}]): list of benchmarks
-            for each material
-        model_score_dict ({str: float}): model score dictionary
-            with scores for each model name
-
-    Returns:
-        (float): sum squared error over all the benchmarks
-
-    """
-    sse = 0
-    for material, benchmark in zip(materials, benchmarks):
-        for symbol, value in benchmark.items():
-            agg = aggregate_quantities(material[symbol], model_score_dict)
-            sse += (agg.magnitude - benchmark[symbol]) ** 2
-    return sse
--- propnet/core/tests/test_quantity.py
+++ propnet/core/tests/test_quantity.py
@@ -1,21 +1,16 @@
 import unittest
 import os
-import pandas as pd
 
 import numpy as np
 
 from pymatgen.util.testing import PymatgenTest
-from propnet.core.quantity import Quantity, get_sse, get_weight, \
-    aggregate_quantities, fit_model_scores
 from propnet.core.symbols import Symbol
 from propnet.core.exceptions import SymbolConstraintError
-from propnet.core.graph import Graph
-from propnet.core.materials import Material
-from propnet.core.provenance import ProvenanceElement
 from propnet import ureg
 
 
 TEST_DIR = os.path.dirname(os.path.abspath(__file__))
+
 
 class QuantityTest(unittest.TestCase):
     def setUp(self):
@@ -78,51 +73,3 @@ class QuantityTest(unittest.TestCase):
             print(q.units)
         with self.assertRaises(ValueError):
             print(q.magnitude)
-
-
-class FittingTests(unittest.TestCase):
-    def setUp(self):
-        path = os.path.join(TEST_DIR, "fitting_test_data.csv")
-        test_data = pd.read_csv(path)
-        graph = Graph()
-        materials = [Material([Quantity("band_gap", bg)])
-                     for bg in test_data['band_gap']]
-        self.evaluated = [graph.evaluate(mat) for mat in materials]
-        self.benchmarks = [{"refractive_index", n}
-                           for n in test_data['refractive_index']]
-
-    def test_get_sse(self):
-        mats = [Material(Quantity("band_gap", n)) for n in range(1, 5)]
-        benchmarks = [{"band_gap": 1.1*n} for n in range(1, 5)]
-        err = get_sse(mats, benchmarks)
-        test_val = sum([0.01*n**2 for n in range(1, 5)])
-        self.assertEqual(err, test_val)
-        # Big dataset
-        err = get_sse(self.evaluated, self.benchmarks)
-        self.assertEqual(err, 123.01)
-
-    def test_get_weight(self):
-        q1 = Quantity("band_gap", 3.2)
-        wt = get_weight(q1)
-        self.assertEqual(wt, 1)
-        p2 = ProvenanceElement(model="model_2", inputs=[q1])
-        q2 = Quantity("refractive_index", 4, provenance=p2)
-        wt2 = get_weight(q2, {"model_2": 0.5})
-        self.assertEqual(wt2, 0.5)
-        p3 = ProvenanceElement(model="model_3", inputs=[q2])
-        q3 = Quantity("bulk_modulus", 100, provenance=p3)
-        wt3 = get_weight(q3, {"model_3": 0.25, "model_2": 0.5})
-        self.assertEqual(wt3, 0.125)
-
-    def test_fit_model_scores(self):
-        scores = fit_model_scores(self.evaluated, self.benchmarks)
-        self.assertEqual(scores['bug'])
-
-    def dumb_test(self):
-        from propnet.core.graph import Graph
-        from propnet.ext.matproj import MPRester
-        mpr = MPRester()
-        g = Graph()
-        mat = mpr.get_material_for_mpid("mp-66")
-        mat.add_default_quantities()
-        g.evaluate(mat)
