12ea99f28cc582d8760754a603ea4691cb796d7d
Matthew Horton <mkhorton@lbl.gov>
2018-11-11 19:14:22 -0800

    Add installation note

--- propnet/web/layouts_home.py
+++ propnet/web/layouts_home.py
@@ -9,6 +9,10 @@ def home_layout():
 **Not intended for public use at this time.**
 Internal collaborators only. Please report bugs :)
 
+Our Github repository is available [here](https://github.com/materialsintelligence/propnet),
+if you would like to test propnet locally please clone the repository, `pip install -r requirements.txt`
+and `python setup.py develop`. 
+
 Funded by: Accelerated Materials Design and Discovery, Toyota Research Institute
     
 """)
