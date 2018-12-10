ae3d78e2e195f0205811e2b9db35b27682797df3
Christian Legaspi <1392774+clegaspi@users.noreply.github.com>
2018-12-10 09:59:05 -0800

    Fix context manager. Create separate stream/logger for prints

--- propnet/core/utils.py
+++ propnet/core/utils.py
@@ -1,7 +1,7 @@
 import os, sys
 from monty.serialization import loadfn, dumpfn
 from habanero.cn import content_negotiation
-from propnet import logger
+from propnet import print_logger, print_stream
 
 _REFERENCE_CACHE_PATH = os.path.join(os.path.dirname(__file__),
                                      '../data/reference_cache.json')
@@ -51,25 +51,42 @@ def references_to_bib(refs):
 
 class PrintToLogger:
     """
-    This class provides a context manager to redirect stdout and stderr to the propnet
-    info logger. This way any print statements received from user-implemented functions
+    This class provides a context manager to redirect stdout to a logger (propnet.print_logger).
+    This way any print statements received from user-implemented functions
     can be automatically logged instead of being printed to the screen.
 
     Usage example:
-        my_method()     # Statement which will not suppress output
+        my_method()     # Statement whose output is not suppressed
 
         with PrintToLogger():
             foo()  # Some statement(s) which may produce screen output to suppress
 
-        some_other_method()     # Statement which will not suppress output
+        some_other_method()     # Statement whose output is not suppressed
+        log = PrintToLogger.get_print_log() # Record anything printed by foo()
     """
+
+    def __init__(self):
+        self.logger = print_logger
+
+    def write(self, buf):
+        for line in buf.rstrip().splitlines():
+            self.logger.info(line.rstrip())
+
+    @staticmethod
+    def get_print_log():
+        """
+        Gets contents of print log, containing any text printed to the screen while
+        running under a PrintToLogger context.
+
+        Returns: (str) contents of print log
+
+        """
+        return print_stream.getvalue()
 
     def __enter__(self):
         self._original_stdout = sys.stdout
-        self._original_stderr = sys.stderr
-        sys.stdout = logger.info
-        sys.stderr = logger.info
+        sys.stdout = self
 
     def __exit__(self, exc_type, exc_val, exc_tb):
         sys.stdout = self._original_stdout
-        sys.stderr = self._original_stderr
+
