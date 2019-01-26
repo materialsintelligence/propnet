11d447767c3b65c4e0a7a392b15c05bcf11fea3f
Shyam Dwaraknath <shyamd@lbl.gov>
2019-01-25 16:08:19 -0800

    Fix lots of problems

--- propnet/core/registry.py
+++ propnet/core/registry.py
@@ -9,7 +9,18 @@ import abc
 class RegistryMeta(abc.ABCMeta):
     def __init__(cls, name, bases, nmspc):
         super(RegistryMeta, cls).__init__(name, bases, nmspc)
-        cls.all_instances = dict()
+
+        # This ensures we have a master registry at the root class so anything
+        # inheriting from Model will register to Model
+        cls.all_instances = None
+        for b in bases:
+            if isinstance(b, RegistryMeta):
+                cls.all_instances = b.all_instances
+                break
+
+        # If we didn't find master registry then make a new one
+        if cls.all_instances == None:
+            cls.all_instances = dict()
 
     def __call__(cls, *args, **kwargs):
 
@@ -17,14 +28,25 @@ class RegistryMeta(abc.ABCMeta):
         if "name" in kwargs:
             name = kwargs["name"]
         elif len(args) < 1:
-            raise TypeError("Can't get a propnet object without a name")
+            raise ValueError("Can't get a propnet object without a name")
         else:
             name = args[0]
 
         # If there isn't already an instance with this name
         # Let's make a new object
-        if name not in cls.all_instances:
+        if name in cls.all_instances:
+            if getattr(cls, "fail_on_duplicate", False):
+                raise ValueError(f"Can't instantiate more than one of {cls.__name__} named {name}")
+            else:
+                return cls.all_instances[name]
+        else:
             new_object = super(RegistryMeta, cls).__call__(*args, **kwargs)
-            cls.all_instances[id] = new_object
+            cls.all_instances[name] = new_object
 
-        return cls.all_instances[id]
+        return cls.all_instances[name]
+
+    def __getitem__(cls, name):
+        if name in cls.all_instances:
+            return cls.all_instances[name]
+        else:
+            raise ValueError("No instances of {cls.__name__} named {name}")
