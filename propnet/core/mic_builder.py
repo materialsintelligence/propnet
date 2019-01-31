ab58482badaf37a7c2ad4ac7cfa5fdc71a5453eb
Christian Legaspi <1392774+clegaspi@users.noreply.github.com>
2019-01-30 17:34:21 -0800

    Add mp lookup for scalar properties

--- propnet/core/mic_builder.py
+++ propnet/core/mic_builder.py
@@ -3,43 +3,92 @@ from itertools import combinations_with_replacement
 import numpy as np
 import json
 from minepy import MINE
+from collections import defaultdict
+from propnet.symbols import DEFAULT_SYMBOLS
+from propnet import ureg
 
+import random
 
 class MicBuilder(Builder):
 
-    def __init__(self, data_store, covariance_store, out_file, **kwargs):
+    def __init__(self, propnet_store, mp_store, correlation_store, out_file, **kwargs):
 
-        self.data_store = data_store
-        self.covariance_store = covariance_store
+        self.propnet_store = propnet_store
+        self.mp_store = mp_store
+        self.correlation_store = correlation_store
         self.out_file = out_file
 
-        super(MicBuilder, self).__init__(sources=[data_store],
-                                         targets=[covariance_store],
+        super(MicBuilder, self).__init__(sources=[propnet_store, mp_store],
+                                         targets=[correlation_store],
                                          **kwargs)
 
     def get_items(self):
-        props = ["sound_velocity_transverse", "sound_velocity_longitudinal", "debye_temperature",
-                 "ionic_radius_b", "volume_unit_cell", "lame_first_parameter", "shear_modulus",
-                 "thermal_conductivity", "pugh_ratio", "poisson_ratio", "hhi_production",
-                 "band_gap_gw", "melting_point", "relative_permeability",
-                 "dielectric_figure_of_merit_current_leakage", "nsites", "molar_mass",
-                 "vickers_hardness", "cost_per_kg", "hhi_reserve", "density", "refractive_index",
-                 "dielectric_figure_of_merit_energy", "atomic_density", "interatomic_spacing",
-                 "ionic_radius_a", "mass_per_atom", "p_wave_modulus", "band_gap_pbe", "cost_per_mol",
-                 "bulk_modulus", "sound_velocity_mean", "volume_per_atom", "gruneisen_parameter",
-                 "youngs_modulus", "band_gap"]
-        data = self.data_store.query(
-            criteria={},
-            properties=[p + '.mean' for p in props])
-        data = [d for d in data]
+        data = defaultdict(dict)
+        propnet_props = [v.name for v in DEFAULT_SYMBOLS.values()
+                         if (v.category == 'property' and v.shape == 1)]
 
-        for prop_a, prop_b in combinations_with_replacement(props, 2):
+        # Debug with subset of mpids
+        # mpids = self.propnet_store.query(criteria={}, properties=['task_id'])
+        # mpids = [m['task_id'] for m in mpids]
+        # count = 0
+        # mpids_to_process = []
+        # while count < 1000:
+        #     idx = random.randint(0, len(mpids)-1)
+        #     mpids_to_process.append(mpids.pop(idx))
+        #     count += 1
+        #
+        # propnet_data = self.propnet_store.query(
+        #     criteria={'task_id': {'$in': mpids_to_process}},
+        #     properties=propnet_props.copy().extend(['task_id', 'inputs']))
+
+        propnet_data = self.propnet_store.query(
+            criteria={},
+            properties=propnet_props.copy().extend(['task_id', 'inputs']))
+
+        for material in propnet_data:
+            for prop, values in material.items():
+                if prop in propnet_props:
+                    data[material['task_id']][prop] = values['mean']
+                elif prop == 'inputs':
+                    input_d = defaultdict(list)
+                    for q in values:
+                        if q['symbol_type'] in propnet_props:
+                            this_q = ureg.Quantity(q['value'], q['units'])
+                            input_d[q['symbol_type']].append(this_q)
+                    data[material['task_id']].update(
+                        {k: sum(v) / len(v) for k, v in input_d.items()})
+
+        mp_props = ["eij_max", "elastic_anisotropy", "universal_anisotropy",
+                    "poly_electronic", "total_magnetization", "efermi",
+                    "total_magnetization_normalized_vol"]
+
+        # mp_data = self.mp_store.query(
+        #     criteria={'task_id': {'$in': mpids_to_process}},
+        #     properties=mp_props.copy().extend('task_id')
+        # )
+
+        mp_data = self.mp_store.query(
+            criteria={},
+            properties=mp_props.copy().extend('task_id')
+        )
+
+        for material in mp_data:
+            for prop, value in material.items():
+                if prop in mp_props:
+                    data[material['task_id']][prop] = value
+
+        for prop_a, prop_b in combinations_with_replacement(propnet_props + mp_props, 2):
             x = []
             y = []
-            for d in data:
-                if prop_a in d.keys() and prop_b in d.keys():
-                    x.append(d[prop_a]['mean'])
-                    y.append(d[prop_b]['mean'])
+            for props_data in data.values():
+                if prop_a in props_data.keys() and prop_b in props_data.keys():
+                    x.append(props_data[prop_a])
+                    y.append(props_data[prop_b])
+
+            if prop_a in propnet_props:
+                x = [xx.to(DEFAULT_SYMBOLS[prop_a].units).magnitude if isinstance(xx, ureg.Quantity) else xx for xx in x]
+            if prop_b in propnet_props:
+                y = [yy.to(DEFAULT_SYMBOLS[prop_b].units).magnitude if isinstance(yy, ureg.Quantity) else yy for yy in y]
 
             data_dict = {prop_a: x,
                          prop_b: y}
@@ -67,7 +116,7 @@ class MicBuilder(Builder):
                          'property_b': prop_b,
                          'covariance': covariance,
                          'id': hash(prop_a) ^ hash(prop_b)})
-        self.covariance_store.update(data, key='id')
+        self.correlation_store.update(data, key='id')
 
     def finalize(self, cursor=None):
         if self.out_file:
@@ -78,7 +127,7 @@ class MicBuilder(Builder):
         super(MicBuilder, self).finalize(cursor)
 
     def get_properties_matrix(self):
-        data = self.covariance_store.query(criteria={'property_a': {'$exists': True}})
+        data = self.correlation_store.query(criteria={'property_a': {'$exists': True}})
         data = [d for d in data]
         props = list(set(d['property_a'] for d in data))
         matrix = np.zeros(shape=(len(props), len(props))).tolist()
