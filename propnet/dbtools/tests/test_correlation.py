00b943eae2331d63fa3ec145efd3fe32f3c67bfb
Christian Legaspi <1392774+clegaspi@users.noreply.github.com>
2019-02-06 16:27:05 -0800

    Made changes to test correlation between both x, y and y, x because RANSAC and Theil-Sen seem to be more sensitive to it than the other algorithms. This will make build times longer, but hopefully more robust.

--- propnet/dbtools/tests/test_correlation.py
+++ propnet/dbtools/tests/test_correlation.py
@@ -29,6 +29,21 @@ class CorrelationTest(unittest.TestCase):
         self.mp_query_props = ["magnetism.total_magnetization_normalized_vol"]
         self.mp_props = ["total_magnetization_normalized_vol"]
 
+        # vickers hardness (x-axis) vs. bulk modulus (y-axis)
+        self.correlation_values_vickers_bulk = {
+            'linlsq': 0.4155837083845686,
+            'pearson': 0.6446578227126143,
+            'mic': 0.5616515521782413,
+            'theilsen': 0.4047519736540858,
+            'ransac': 0.3747245847179631}
+
+        self.correlation_values_bulk_vickers = {
+            'linlsq': 0.4155837083845686,
+            'pearson': 0.6446578227126143,
+            'mic': 0.5616515521782413,
+            'theilsen': 0.39860109570815505,
+            'ransac': 0.3119656700613579}
+
     def test_serial_runner(self):
         builder = CorrelationBuilder(self.propstore, self.materials, self.correlation)
         runner = Runner([builder])
@@ -41,7 +56,7 @@ class CorrelationTest(unittest.TestCase):
 
     def test_process_item(self):
         test_props = [['band_gap_pbe', 'total_magnetization_normalized_vol'],
-                      ['vickers_hardness', 'bulk_modulus']]
+                      ['bulk_modulus', 'vickers_hardness']]
         linlsq_correlation_values = [0.03620401274778131, 0.4155837083845686]
         path_lengths = [None, 2]
 
@@ -50,15 +65,18 @@ class CorrelationTest(unittest.TestCase):
             builder = CorrelationBuilder(self.propstore, self.materials, self.correlation,
                                          props=props)
             processed = None
+            prop_x, prop_y = props
             for item in builder.get_items():
-                if item['x_name'] != item['y_name']:
+                if item['x_name'] == prop_x and \
+                        item['y_name'] == prop_y:
                     processed = builder.process_item(item)
                     break
 
             self.assertIsNotNone(processed)
             self.assertIsInstance(processed, tuple)
-            prop_a, prop_b, correlation, func_name, n_points, path_length = processed
-            self.assertListEqual([prop_a, prop_b], props)
+            px, py, correlation, func_name, n_points, path_length = processed
+            self.assertEqual(px, prop_x)
+            self.assertEqual(py, prop_y)
             self.assertAlmostEqual(correlation, expected_correlation_val)
             self.assertEqual(func_name, 'linlsq')
             self.assertEqual(n_points, 200)
@@ -68,13 +86,8 @@ class CorrelationTest(unittest.TestCase):
         def custom_correlation_func(x, y):
             return 0.5
 
-        correlation_values = {
-            'linlsq': 0.4155837083845686,
-            'pearson': 0.6446578227126143,
-            'mic': 0.5616515521782413,
-            'theilsen': 0.4047519736540858,
-            'ransac': 0.3747245847179631,
-            'propnet.dbtools.tests.test_correlation.custom_correlation_func': 0.5}
+        correlation_values = {k: v for k, v in self.correlation_values_bulk_vickers.items()}
+        correlation_values['propnet.dbtools.tests.test_correlation.custom_correlation_func'] = 0.5
 
         builder = CorrelationBuilder(self.propstore, self.materials, self.correlation,
                                      props=['vickers_hardness', 'bulk_modulus'],
@@ -84,15 +97,48 @@ class CorrelationTest(unittest.TestCase):
                          msg="Are there new built-in functions in the correlation builder?")
 
         for item in builder.get_items():
-            if item['x_name'] != item['y_name']:
+            if item['x_name'] == 'bulk_modulus' and \
+                    item['y_name'] == 'vickers_hardness':
                 processed = builder.process_item(item)
                 self.assertIsInstance(processed, tuple)
-                prop_a, prop_b, correlation, func_name, n_points, path_length = processed
-                self.assertListEqual([prop_a, prop_b], ['vickers_hardness', 'bulk_modulus'])
+                prop_x, prop_y, correlation, func_name, n_points, path_length = processed
+                self.assertEqual(prop_x, 'bulk_modulus')
+                self.assertEqual(prop_y, 'vickers_hardness')
                 self.assertIn(func_name, correlation_values.keys())
-                # self.assertAlmostEqual(correlation, correlation_values[func_name])
+                self.assertAlmostEqual(correlation, correlation_values[func_name])
                 self.assertEqual(n_points, 200)
                 self.assertEqual(path_length, 2)
+
+    def test_database_write(self):
+        builder = CorrelationBuilder(self.propstore, self.materials, self.correlation,
+                                     props=self.propnet_props + self.mp_props,
+                                     funcs='all')
+
+        runner = Runner([builder])
+        runner.run()
+
+        data = list(self.correlation.query(criteria={}))
+        # count = n_props**2 * n_funcs
+        # n_props = 4, n_funcs = 5
+        self.assertEqual(len(data), 80)
+
+        for d in data:
+            self.assertIsInstance(d, dict)
+            self.assertEqual(set(d.keys()), {'property_x', 'property_y',
+                                             'correlation', 'correlation_func',
+                                             'n_points', 'shortest_path_length',
+                                             'id', '_id', 'last_updated'})
+            self.assertEqual(d['n_points'], 200)
+            if d['property_x'] == 'vickers_hardness' and \
+                    d['property_y'] == 'bulk_modulus':
+                self.assertAlmostEqual(
+                    d['correlation'],
+                    self.correlation_values_vickers_bulk[d['correlation_func']])
+            elif d['property_x'] == 'bulk_modulus' and \
+                    d['property_y'] == 'vickers_hardness':
+                self.assertAlmostEqual(
+                    d['correlation'],
+                    self.correlation_values_bulk_vickers[d['correlation_func']])
 
     # Just here for reference, in case anyone wants to create a new set
     # of test materials. Requires mongogrant read access to knowhere.lbl.gov.
