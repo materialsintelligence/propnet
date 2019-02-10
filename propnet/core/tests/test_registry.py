3548344e523f3894f411ec7c7d7f4db51252bc02
Shyam Dwaraknath <shyamd@lbl.gov>
2019-02-10 13:42:41 -0800

    Update registry to be simple container

--- propnet/core/tests/test_registry.py
+++ propnet/core/tests/test_registry.py
@@ -1,48 +1,17 @@
 import unittest
 
-from propnet.core.registry import RegistryMeta
-
-
-class registerable(metaclass=RegistryMeta):
-    fail_on_duplicate = False
-
-    def __init__(self, name=None):
-        self.name = name
-
-
-class registerable_no_duplicates(registerable):
-    fail_on_duplicate = True
-
-    def __init__(self, name):
-        self.name = name
-
-
-class registerable_multi_inheritance(type, registerable):
-    pass
+from propnet.core.registry import Registry
 
 
 class RegistryTest(unittest.TestCase):
     def test_basic_registry(self):
-        for i in range(10):
-            name = str(i)
-            registerable(name)
+        test_reg = Registry("test")
+        test_reg2 = Registry("test")
+        test_reg3 = Registry("test2")
 
-        for i in range(10):
-            # Ensure registry has the names
-            self.assertIn(str(i), registerable.all_instances.keys())
-            # Ensure registry is accessible via index operation
-            self.assertTrue(isinstance(registerable[str(i)], registerable))
-
-        with self.assertRaises(ValueError):
-            registerable()
-
-    def test_registry_inheritance(self):
-
-        with self.assertRaises(ValueError):
-            registerable_no_duplicates("1")
-            registerable_no_duplicates("1")
-
-        self.assertEqual(registerable_no_duplicates.all_instances, registerable.all_instances)
+        self.assertIsInstance(test_reg, dict)
+        self.assertTrue(test_reg is test_reg2)
+        self.assertTrue(test_reg is not test_reg3)
 
 
 if __name__ == "__main__":
