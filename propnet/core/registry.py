51780a003ed807fe6662eb76f00c182efe376b57
Shyam Dwaraknath <shyamd@lbl.gov>
2019-02-10 13:42:41 -0800

    Update registry to be simple container

--- propnet/core/registry.py
+++ propnet/core/registry.py
@@ -7,46 +7,34 @@ import abc
 
 
 class RegistryMeta(abc.ABCMeta):
+
+    all_instances = None
+
     def __init__(cls, name, bases, nmspc):
         super(RegistryMeta, cls).__init__(name, bases, nmspc)
 
-        # This ensures we have a master registry at the root class so anything
-        # inheriting from Model will register to Model
-        cls.all_instances = None
-        for b in bases:
-            if isinstance(b, RegistryMeta):
-                cls.all_instances = b.all_instances
-                break
-
         # If we didn't find master registry then make a new one
-        if cls.all_instances == None:
+        if cls.all_instances is None:
             cls.all_instances = dict()
 
     def __call__(cls, *args, **kwargs):
-
-        # First get the name
         if "name" in kwargs:
+            # First get the name
             name = kwargs["name"]
-        elif len(args) < 1:
-            raise ValueError("Can't get a propnet object without a name")
-        else:
+            kwargs.pop("name",None)
+        elif len(args) > 0:
+            # assume first arg is name
             name = args[0]
-
-        # If there isn't already an instance with this name
-        # Let's make a new object
-        if name in cls.all_instances:
-            if getattr(cls, "fail_on_duplicate", False):
-                raise ValueError(f"Can't instantiate more than one of {cls.__name__} named {name}")
-            else:
-                return cls.all_instances[name]
+            args = args[1:]
         else:
+            raise ValueError("Must specify a name to get a Registry")
+
+        if name not in cls.all_instances:
             new_object = super(RegistryMeta, cls).__call__(*args, **kwargs)
             cls.all_instances[name] = new_object
 
         return cls.all_instances[name]
 
-    def __getitem__(cls, name):
-        if name in cls.all_instances:
-            return cls.all_instances[name]
-        else:
-            raise ValueError("No instances of {cls.__name__} named {name}")
+
+class Registry(dict, metaclass=RegistryMeta):
+    pass
