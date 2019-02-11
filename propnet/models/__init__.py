04a696c53e3b84255694b24a5f6ad40397e44f1d
Shyam Dwaraknath <shyamd@lbl.gov>
2019-02-11 14:29:57 -0800

    Remove built in dicts in main models and symbols modules

--- propnet/models/__init__.py
+++ propnet/models/__init__.py
@@ -1,15 +1,6 @@
 from propnet.models import serialized, python, composite
 from propnet.core.registry import Registry
 
-DEFAULT_MODEL_DICT = Registry("models")
-COMPOSITE_MODEL_DICT = Registry("composite_models")
-
-COMPOSITE_MODEL_NAMES = list(COMPOSITE_MODEL_DICT.keys())
-DEFAULT_MODEL_NAMES = list(DEFAULT_MODEL_DICT.keys())
-
-DEFAULT_MODELS = list(DEFAULT_MODEL_DICT.values())
-DEFAULT_COMPOSITE_MODELS = list(COMPOSITE_MODEL_DICT.values())
-
 # This is just to enable importing the model directly from this module for example code generation
-for name, model in DEFAULT_MODEL_DICT.items():
+for name, model in Registry("models").items():
     globals()[name] = model
--- propnet/symbols/__init__.py
+++ propnet/symbols/__init__.py
@@ -9,9 +9,6 @@ from propnet.core.registry import Registry
 
 # stores all loaded properties as PropertyMetadata instances in a dictionary,
 # mapped to their names
-DEFAULT_SYMBOLS = Registry("symbols")
-DEFAULT_SYMBOL_VALUES = Registry("symbol_values")
-DEFAULT_UNITS = Registry("units")
 
 _DEFAULT_SYMBOL_TYPE_FILES = glob(
     os.path.join(os.path.dirname(__file__), '../symbols/**/*.yaml'),
@@ -19,17 +16,16 @@ _DEFAULT_SYMBOL_TYPE_FILES = glob(
 
 for f in _DEFAULT_SYMBOL_TYPE_FILES:
     symbol_type = Symbol.from_dict(loadfn(f))
-    DEFAULT_SYMBOLS[symbol_type.name] = symbol_type
+    Registry("symbols")[symbol_type.name] = symbol_type
     if symbol_type.default_value is not None:
-        DEFAULT_SYMBOL_VALUES[symbol_type] = symbol_type.default_value
+        Registry("symbol_values")[symbol_type] = symbol_type.default_value
     if "{}.yaml".format(symbol_type.name) not in f:
         raise ValueError('Name/filename mismatch in {}'.format(f))
 
 # Stores all loaded properties' names in a tuple in the global scope.
-DEFAULT_UNITS.update({name: symbol.units
-                      for name, symbol in DEFAULT_SYMBOLS.items()})
-DEFAULT_SYMBOL_TYPE_NAMES = tuple(DEFAULT_SYMBOLS.keys())
+Registry("units").update({name: symbol.units
+                      for name, symbol in Registry("symbols").items()})
 
 # This is just to enable importing this module
-for name, symbol in DEFAULT_SYMBOLS.items():
+for name, symbol in Registry("symbols").items():
     globals()[name] = symbol
