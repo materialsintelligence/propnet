a2518209930a9e3188a891f16c16b392d7a57809
Christian Legaspi <1392774+clegaspi@users.noreply.github.com>
2019-04-25 17:04:48 -0700

    Rework ingester to use AFLUX to get auids and urls, then grab json from url

--- propnet/dbtools/aflow.py
+++ propnet/dbtools/aflow.py
@@ -4,41 +4,36 @@ from maggma.builders import Builder
 from monty.json import jsanitize
 from pymongo import InsertOne
 import logging
+import requests
+from requests.adapters import HTTPAdapter
+from urllib3.util import Retry
 
-# noinspection PyUnresolvedReferences
-import propnet.dbtools.aflow_redefs
 
-# Issues with the AFLUX API or the python wrapper
-unavailable_aflux_keys = ('ael_elastic_anistropy', 'Pullay_stress',
-                          'aflowlib_entries', 'aflowlib_entries_number',
-                          'author', 'catalog', 'corresponding', 'data_language',
-                          'keywords', 'pressure_final', 'pressure_residual', 'sponsor')
+# Populate all available kws from AFLOW
+AFLOW_KWS = dict()
+load(AFLOW_KWS)
+AFLOW_KWS = list(AFLOW_KWS.keys())
 
 logger = logging.getLogger(__name__)
 
 
 class AFLOWIngester(Builder):
     def __init__(self, data_target, auid_target,
-                 keywords=None, batch_size=1000, insert_only=False,
+                 keywords=None, auid_batch_size=1000, insert_only=False,
                  **kwargs):
         self.data_target = data_target
         self.auid_target = auid_target
-        if batch_size > 1000:
+        if auid_batch_size > 1000:
             logger.warning("It is recommended to keep batch_size <= 1000 "
-                           "or the API request may be rejected by the server.")
-        self.batch_size = batch_size
+                           "or the AFLUX API request may be rejected by the server.")
+        self.auid_batch_size = auid_batch_size
         self.total = None
         self.insert_only = insert_only
-
-        self._available_kws = dict()
-        load(self._available_kws)
-        self._available_kws = {k: v for k, v in self._available_kws.items()
-                               if k not in unavailable_aflux_keys}
         
-        bad_kw_check = [k for k in keywords or [] if k not in self._available_kws]
+        bad_kw_check = [k for k in keywords or [] if k not in AFLOW_KWS]
         if len(bad_kw_check) != 0:
             raise KeyError("Bad keywords:\n{}".format(bad_kw_check))
-        self.keywords = keywords or list(self._available_kws.keys())
+        self.keywords = keywords
         
         super(AFLOWIngester, self).__init__(sources=[], targets=[data_target, auid_target],
                                             **kwargs)
@@ -50,17 +45,34 @@ class AFLOWIngester(Builder):
         afs.filter()
         '''
         afs = AsyncQuery.from_pymongo(criteria={},
-                                      properties=self.keywords,
-                                      request_size=self.batch_size)
+                                      properties=['auid', 'compound', 'aurl'],
+                                      request_size=self.auid_batch_size)
         self.total = afs.N
         yield from afs
             
     def process_item(self, item):
-        d = {k: item.attributes.get(k, None) for k in self.keywords}
-        d = {k: v for k, v in d.items() if v is not None}
-        auid_info = {k: d[k] for k in ('auid', 'compound', 'aurl')}
-        
-        return jsanitize(auid_info, strict=True), jsanitize(d, strict=True)
+        url: str = item.aurl
+        url = url.replace(":", "/")
+        if not url.startswith("http://"):
+            url = "http://" + url
+
+        auid_info = {k: getattr(item, k, None) for k in ('auid', 'compound', 'aurl')}
+
+        if url is not None:
+            s = requests.Session()
+            retries = Retry(total=5, backoff_factor=1)
+            s.mount('http://', HTTPAdapter(max_retries=retries))
+            response = requests.get(url, params={'format': 'json'})
+
+            if response.ok:
+                data = jsanitize(response.json())
+            else:
+                data = jsanitize({'error': response.text}, strict=True)
+                data.update(auid_info)
+        else:
+            data = {'error': 'No URL'}
+
+        return auid_info, data
     
     def update_targets(self, items):
         if self.insert_only:
