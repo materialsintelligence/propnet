2079cf8738b069e1155ab55c15b0ef641408483a
Christian Legaspi <1392774+clegaspi@users.noreply.github.com>
2019-05-17 15:57:31 -0700

    Add option to exclude deprecated materials

--- propnet/dbtools/mp_builder.py
+++ propnet/dbtools/mp_builder.py
@@ -28,7 +28,8 @@ class PropnetBuilder(MapBuilder):
     """
 
     def __init__(self, materials, propstore, materials_symbol_map=None,
-                 criteria=None, source_name="", graph_parallel=False,
+                 criteria=None, source_name="", include_deprecated=False,
+                 graph_parallel=False,
                  max_graph_workers=None, graph_timeout=None,
                  allow_child_process=False, **kwargs):
         """
@@ -40,6 +41,11 @@ class PropnetBuilder(MapBuilder):
             criteria (dict): criteria for Mongodb find() query specifying
                 criteria for records to process
             source_name (str): identifier for record source
+            include_deprecated (bool): True processes materials marked as
+                deprecated via the "deprecated" field. False skips those materials.
+                If an entry does not have the "deprecated" field, it will be processed.
+                Note that False will create a logical "and" with any criteria specified
+                in "criteria". Default: False
             graph_parallel (bool): True runs the graph algorithm in parallel with
                 the number of workers specified by max_workers. Default: False (serial)
                 Note: there will be no substantial speed-up from using a parallel
@@ -64,6 +70,17 @@ class PropnetBuilder(MapBuilder):
         self.materials = materials
         self.propstore = propstore
         self.criteria = criteria
+        self.include_deprecated = include_deprecated
+        if not include_deprecated:
+            deprecated_filter = {
+                "$or": [{"deprecated": {"$exists": False}},
+                        {"deprecated": False}]
+            }
+            if criteria:
+                self.criteria = {'$and': [criteria, deprecated_filter]}
+            else:
+                self.criteria = deprecated_filter
+
         self.materials_symbol_map = materials_symbol_map \
                                     or MPRester.mapping
         if source_name == "":
@@ -89,6 +106,7 @@ class PropnetBuilder(MapBuilder):
 
         super(PropnetBuilder, self).__init__(source=materials,
                                              target=propstore,
+                                             query=self.criteria,
                                              ufn=self.process,
                                              projection=props,
                                              **kwargs)
