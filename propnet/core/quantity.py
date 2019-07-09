42297644f1139119af25cae1877f58a2fe6c4a9f
Christian Legaspi <1392774+clegaspi@users.noreply.github.com>
2019-07-09 12:28:23 -0700

    Downgrade logger warning for no units supplied to info

--- propnet/core/quantity.py
+++ propnet/core/quantity.py
@@ -473,7 +473,7 @@ class NumQuantity(BaseQuantity):
             if symbol_type.units is None:
                 raise ValueError("No units specified as keyword and no "
                                  "units provided by symbol for NumQuantity")
-            logger.warning("WARNING: No units supplied, assuming default units from symbol.")
+            logger.info("WARNING: No units supplied, assuming default units from symbol.")
 
         units = units or symbol_type.units
 
